04be606977bcd458a8682ae8a33dd92a08fe2c8d
thejackshelton <me@jackshelton.com>
2024-04-11 21:10:56 -0500

    first commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,28 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectListbox,
+  SelectOption,
+} from "@qwik-ui/headless";
+import styles from "~/components/select/select-example.css?inline";
 
 export default component$(() => {
+  useStyles$(styles);
+
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <Select class="select">
+        <SelectTrigger class="select-trigger">
+          <SelectValue placeholder="Select an option" />
+        </SelectTrigger>
+        <SelectListbox class="select-listbox">
+          <SelectOption>Option 1</SelectOption>
+          <SelectOption>Option 2</SelectOption>
+        </SelectListbox>
+      </Select>
     </>
   );
 });
