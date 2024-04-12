3ffad7e95878a5cafa194528438c7fc39f381351
thejackshelton <me@jackshelton.com>
2024-04-11 21:46:29 -0500

    first commit

--- src/components/select/index.ts
+++ src/components/select/index.ts
@@ -1,9 +1,8 @@
-export * from "./select";
-export * from "./select-inline";
-export * from "./select-listbox";
-export * from "./select-option";
-export * from "./select-trigger";
-export * from "./select-value";
-export * from "./select-group";
-export * from "./select-label";
-export * from "./hidden-select";
+export { Select as Root } from "./select-inline";
+export { SelectListbox as Listbox } from "./select-listbox";
+export { SelectOption as Option } from "./select-option";
+export { SelectTrigger as Trigger } from "./select-trigger";
+export { SelectValue as Value } from "./select-value";
+export { SelectGroup as Group } from "./select-group";
+export { SelectLabel as Label } from "./select-label";
+export { HiddenSelect as Hidden } from "./hidden-select";
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,27 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectListbox,
-  SelectOption,
-} from "@qwik-ui/headless";
+import { Select } from "~/components";
 import styles from "~/components/select/select-example.css?inline";
 
 export default component$(() => {
   useStyles$(styles);
 
+  // Not sure why Qwik core is woken up on page load here in Qwik City, this problem does not happen in Astro. (executes on interaction)
+
+  // If I had to guess, it's likely a bug in the Qwik City service worker or Qwik City. We need to deprecate the Qwik City service worker with the one in core.
+
+  // Run the preview and interact with the listbox and click an option. Write down the total JS executed. Then remove the Listbox and Option, and click the trigger again.
+
   return (
-    <>
-      <Select class="select">
-        <SelectTrigger class="select-trigger">
-          <SelectValue placeholder="Select an option" />
-        </SelectTrigger>
-        <SelectListbox class="select-listbox">
-          <SelectOption>Option 1</SelectOption>
-          <SelectOption>Option 2</SelectOption>
-        </SelectListbox>
-      </Select>
-    </>
+    <Select.Root class="select">
+      <Select.Trigger class="select-trigger">
+        <Select.Value placeholder="Select an option" />
+      </Select.Trigger>
+      <Select.Listbox class="select-listbox">
+        <Select.Option>Option 1</Select.Option>
+        <Select.Option>Option 2</Select.Option>
+      </Select.Listbox>
+    </Select.Root>
   );
 });
 
